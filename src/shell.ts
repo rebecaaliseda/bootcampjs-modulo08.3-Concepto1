5fb3245183d5c510ddf991f80eed735c512d7cd5
Rebeca Aliseda <rebecaaliseda26@gmail.com>
2024-07-08 20:53:47 +0200

    Juego memoria: Barajar Cartas

---
+++ src/shell.ts
@@ -0,0 +1,11 @@
+import { arrayAnimales } from './modelo';
+import { barajarCartas } from './motor';
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log(barajarCartas(arrayAnimales));
+  console.log(barajarCartas(arrayAnimales));
+  console.log(barajarCartas(arrayAnimales));
+  console.log(barajarCartas(arrayAnimales));
+  console.log(barajarCartas(arrayAnimales));
+  console.log(barajarCartas(arrayAnimales));
+});
